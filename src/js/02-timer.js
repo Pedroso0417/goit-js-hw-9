70978e1b7699141765f22da05967be10f742ee7a
Pedroso0417/goit-js-hw-8 <136574676+Pedroso0417@users.noreply.github.com>
2023-12-23 01:26:05 +0800

    Add Commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,81 +1,83 @@
-// Described in documentation
 import flatpickr from 'flatpickr';
-// Additional styles import
 import 'flatpickr/dist/flatpickr.min.css';
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Add leading zero to a number if less than 10
-  function addLeadingZero(value) {
-    return value.toString().padStart(2, '0');
-  }
+// ... (other imports)
 
-  const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      handleDateSelection(selectedDates[0]);
-    },
-  };
+const datetimePickerEl = document.querySelector('#datetime-picker');
+const startButtonEl = document.querySelector('button[data-start]');
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
 
-  flatpickr('#datetime-picker', options);
+startButtonEl.disabled = false;
 
-  const startBtn = document.getElementById('start-btn');
-  const timerElement = document.getElementById('timer');
-  let countdownInterval;
+let timerID; // Variable to store the timer ID
 
-  function handleDateSelection(selectedDate) {
-    const currentDate = new Date();
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const dateNow = Date.now();
 
-    if (selectedDate <= currentDate) {
-      alert('Please choose a date in the future');
-      startBtn.disabled = true;
+    if (selectedDate < dateNow) {
+      window.alert('Please choose a date in the future');
+      startButtonEl.disabled = true;
       return;
     }
 
-    startBtn.disabled = false;
-    startBtn.addEventListener('click', startCountdown);
-  }
+    function handleCountdown() {
+      startButtonEl.disabled = true;
+      datetimePickerEl.disabled = true;
 
-  function startCountdown() {
-    const selectedDate = flatpickr('#datetime-picker').selectedDates[0];
-    const currentDate = new Date();
-    let timeDifference = selectedDate.getTime() - currentDate.getTime();
+      timerID = setInterval(() => {
+        const currentTime = Date.now();
+        if (selectedDate < currentTime) {
+          clearInterval(timerID);
+          datetimePickerEl.disabled = false;
+          return;
+        }
 
-    countdownInterval = setInterval(function () {
-      const remainingTime = convertMs(timeDifference);
-      displayTimer(remainingTime);
+        const timeDifference = selectedDate - currentTime;
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+          (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor(
+          (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-      if (timeDifference <= 0) {
-        clearInterval(countdownInterval);
-        startBtn.disabled = true;
-      }
+        daysEl.textContent = addLeadingZero(days);
+        hoursEl.textContent = addLeadingZero(hours);
+        minutesEl.textContent = addLeadingZero(minutes);
+        secondsEl.textContent = addLeadingZero(seconds);
+      }, 100);
+    }
 
-      timeDifference -= 1000;
-    }, 1000);
-  }
+    startButtonEl.addEventListener('click', handleCountdown);
+  },
+};
 
-  function displayTimer(remainingTime) {
-    const days = addLeadingZero(remainingTime.days);
-    const hours = addLeadingZero(remainingTime.hours);
-    const minutes = addLeadingZero(remainingTime.minutes);
-    const seconds = addLeadingZero(remainingTime.seconds);
+flatpickr('#datetime-picker', options);
 
-    timerElement.textContent = `${days}:${hours}:${minutes}:${seconds}`;
-  }
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-  function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
 
-    return { days, hours, minutes, seconds };
-  }
-});
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
